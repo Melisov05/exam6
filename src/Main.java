4e9e3efee27ec2fd88f0f69f896905b9eddb0b8f
Melisov01 <aytemirmelisov@gmail.com>
2024-03-02 11:46:40 +0600

    basic server created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import server.BasicServer;
+import server.ExamServer;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ExamServer server = new ExamServer("localhost", 9889);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/server/ExamServer.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.io.IOException;
+
+public class ExamServer extends BasicServer{
+
+    public ExamServer(String host, int port) throws IOException {
+        super(host, port);
+    }
+}
