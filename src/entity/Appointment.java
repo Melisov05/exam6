adc7c4ce6cd2954b65dd9657050b8fa91b55f9e9
Melisov01 <aytemirmelisov@gmail.com>
2024-03-02 11:54:02 +0600

    added fields for Patient and Appointment classes

---
+++ src/entity/Appointment.java
@@ -0,0 +1,8 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private LocalDateTime appointmentTime;
+    private Patient patient;
+}
---
+++ src/entity/Patient.java
@@ -0,0 +1,10 @@
+package entity;
+
+import java.time.LocalDate;
+
+public class Patient {
+    private String fullName;
+    private LocalDate dateOfBirth;
+    private String patientType;
+    private String symptoms;
+}
